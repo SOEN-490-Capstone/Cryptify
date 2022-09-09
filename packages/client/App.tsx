6ea2d685d1adb7f865b26eba92586473bc14a5f6
Jason Gerard <jasongerard321@gmail.com>
2022-09-09 19:33:30 -0400

    CRYP-9: Setup react native base with monorepo support (#3)
    * Setup initial React Native project for the client
    
    * Implement monorepo with yarn workspaces
    
    * Refactor webpack and tsconfig files
    
    * Centralized gitignore files
    
    * Integrate api with monorepo
    
    * Finish integrating api with monorepo for auto building

---
+++ packages/client/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+import {ports} from "@cryptify/api/dist/common/src/ports";
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  console.log(ports.CLIENT)
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={colorScheme} />
+        <StatusBar />
+      </SafeAreaProvider>
+    );
+  }
+}

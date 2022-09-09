fd2e88718c4ca082185cfedc0a299ae3d4f364cc
Jason Gerard <jasongerard321@gmail.com>
2022-09-09 12:43:02 -0400

    Implement monorepo with yarn workspaces

--- packages/client/App.tsx
+++ packages/client/App.tsx
@@ -4,11 +4,14 @@ import { SafeAreaProvider } from 'react-native-safe-area-context';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import { hello, User } from '@cryptify/common/src/user';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
+  const user = hello() as User;
+  console.log(user);
   if (!isLoadingComplete) {
     return null;
   } else {
---
+++ packages/common/src/user.ts
@@ -0,0 +1,12 @@
+export interface User {
+    name: string;
+}
+
+export const x = 'test';
+
+export function hello() {
+    console.log(x);
+    return {
+        name: 'Jason'
+    }
+}

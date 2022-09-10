7cf066a2292d4232c2b93e2c1552af21e3bf5621
Jason Gerard <jasongerard321@gmail.com>
2022-09-10 12:39:07 -0400

    CRYP-9: Move eslint and prettier to project root (#4)
    * Refactor eslint and prettier into project root
    
    * Add new scripts to root package.json

--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -1,11 +1,14 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config');
+const createExpoWebpackConfigAsync = require("@expo/webpack-config"); // eslint-disable-line @typescript-eslint/no-var-requires
 
-module.exports = async function(env, argv) {
-    const config = await createExpoWebpackConfigAsync({
-        ...env,
-        babel: {
-            dangerouslyAddModulePathsToTranspile: ['@cryptify']
-        }
-    }, argv);
+module.exports = async function (env, argv) {
+    const config = await createExpoWebpackConfigAsync(
+        {
+            ...env,
+            babel: {
+                dangerouslyAddModulePathsToTranspile: ["@cryptify"],
+            },
+        },
+        argv,
+    );
     return config;
 };

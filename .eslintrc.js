8330bfaf8c0dd12e3ddd8cf4ed0ec2d25f3a65bd
Jason Gerard <jasongerard321@gmail.com>
2022-09-10 12:39:07 -0400

    CRYP-9: Move eslint and prettier to project root (#4)
    * Refactor eslint and prettier into project root
    
    * Add new scripts to root package.json

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+    },
+    extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended",
+    ],
+    rules: {
+        "@typescript-eslint/no-explicit-any": 0,
+    },
+    ignorePatterns: [
+        "dist"
+    ]
+};

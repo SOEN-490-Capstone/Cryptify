4b7a0a35fbf76a30d460311f2f1a990777e53fba
Andre <psnmeee@gmail.com>
2022-09-12 15:27:13 -0400

    remove BaseRepo

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from "@nestjs/common";
 import { BaseService } from "./services/base/base.service";
 import { BaseController } from "./controllers/base/base.controller";
-import { BaseRepository } from "./repositories/base/base.repository";
 
 @Module({
     imports: [],
     controllers: [BaseController],
-    providers: [BaseService, BaseRepository],
+    providers: [BaseService],
 })
 export class AppModule {}

28e73bcf193ca9978493a4fe6e682c4f624c39aa
Jason Gerard <jasongerard321@gmail.com>
2022-09-14 19:42:27 -0400

    CRYP-61: Add diagram generation scripts (#8)
    * Add basic arch diagram and markdown generator
    
    * Add package generation script
    
    * Fix yarn lock

---
+++ packages/diagrams/generate_diagrams.ts
@@ -0,0 +1,9 @@
+const dir = '.';
+import * as fs from 'fs';
+import { exec } from "node:child_process";
+
+fs.readdirSync(dir).forEach(file => {
+    if (file.endsWith(".md")) {
+        exec(`mmdc -i ${dir}/${file} -o ${dir}/generated/${file}`)
+    }
+});

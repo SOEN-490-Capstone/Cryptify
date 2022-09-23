b7b49a67a3d05786ce074967584f09556091212e
Jason Gerard <jasongerard321@gmail.com>
2022-09-23 15:13:32 -0400

    Fix error with crypto math

--- packages/client/gateways/request.ts
+++ packages/client/gateways/request.ts
@@ -1,5 +1,5 @@
 export async function request<T>(path: string, req: any): Promise<T> {
-    const response = await fetch(`http://localhost:3001/${path}`, {
+    const response = await fetch(`http://192.168.86.35:3001/${path}`, {
         method: "POST",
         headers: {
             Accept: "application/json",

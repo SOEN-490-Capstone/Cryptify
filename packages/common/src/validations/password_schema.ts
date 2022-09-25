f10fb0435a39e43cbbebbf8e52d369bb7b14e643
Domenic Seccareccia <dom_seccareccia@hotmail.com>
2022-09-24 21:43:48 -0400

    Added and modified sign in error messages

--- packages/common/src/validations/password_schema.ts
+++ packages/common/src/validations/password_schema.ts
@@ -4,8 +4,10 @@ import * as yup from "yup";
 // has a minimum length of 8 characters
 export const passwordSchema = yup
     .string()
+    .min(6)
+    .max(20, "Password must be between 6 and 20 characters")
     .matches(
         new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()?+=_]).{6,20}$"),
-        "Password must be between 6 and 20 characters",
+        "Password must contain at least 1 upper case, 1 lower case letter, 1 number, and 1 special character.",
     )
     .required("Enter a password.");

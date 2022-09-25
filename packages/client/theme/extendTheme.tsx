e234cdf6a9baaa59fa2ff0d0d4c29ed1ba26de4c
Domenic Seccareccia <dom_seccareccia@hotmail.com>
2022-09-24 23:07:12 -0400

    Removed font config because it will be handled in a future sprint

--- packages/client/theme/extendTheme.tsx
+++ packages/client/theme/extendTheme.tsx
@@ -6,52 +6,6 @@ import { formControlErrorMessage } from "./components/form-control";
 
 export default function () {
     return extendTheme({
-        fontConfig: {
-            Roboto: {
-                100: {
-                    normal: "Roboto-Light",
-                    italic: "Roboto-LightItalic",
-                },
-                200: {
-                    normal: "Roboto-Light",
-                    italic: "Roboto-LightItalic",
-                },
-                300: {
-                    normal: "Roboto-Light",
-                    italic: "Roboto-LightItalic",
-                },
-                400: {
-                    normal: "Roboto-Regular",
-                    italic: "Roboto-Italic",
-                },
-                500: {
-                    normal: "Roboto-Medium",
-                },
-                600: {
-                    normal: "Roboto-Medium",
-                    italic: "Roboto-MediumItalic",
-                },
-                700: {
-                    normal: 'Roboto-Bold',
-                },
-                800: {
-                    normal: 'Roboto-Bold',
-                    italic: 'Roboto-BoldItalic',
-                  },
-                900: {
-                    normal: 'Roboto-Bold',
-                    italic: 'Roboto-BoldItalic',
-                },
-            },
-        },
-
-        // Make sure values below matches any of the keys in `fontConfig`
-        fonts: {
-            heading: "Roboto",
-            body: "Roboto",
-            mono: "Roboto",
-        },
-
         components: {
             Text: text,
             Button : button,

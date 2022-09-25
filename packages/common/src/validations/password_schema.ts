8bdd8ae13583d6a3d3475c64a92722556177c640
Domenic Seccareccia <dom_seccareccia@hotmail.com>
2022-09-24 23:08:09 -0400

    Add missing period to an error message

--- packages/common/src/validations/password_schema.ts
+++ packages/common/src/validations/password_schema.ts
@@ -5,7 +5,7 @@ import * as yup from "yup";
 export const passwordSchema = yup
     .string()
     .min(6)
-    .max(20, "Password must be between 6 and 20 characters")
+    .max(20, "Password must be between 6 and 20 characters.")
     .matches(
         new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z])(?=.*[!@#$%^&*()?+=_]).{6,20}$"),
         "Password must contain at least 1 upper case, 1 lower case letter, 1 number, and 1 special character.",

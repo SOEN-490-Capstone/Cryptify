5044db2340d9587066844905ada33828aa652aa6
Jason Gerard <jasongerard321@gmail.com>
2022-09-29 16:25:58 -0400

    CRYP-13: setup CI/CD pipeline (#31)
    * Add base pipeline and refactor di
    
    * Fix dep issue
    
    * Add in ci.yml file with integration tests
    
    * Test without integration tests
    
    * Fix copy paste issue in yml file
    
    * Fix second part of copy paste issue
    
    * Try to create volume before containers
    
    * Switch to run command
    
    * change folder permissions
    
    * Test with sudo
    
    * Make docker compose non-interactive

--- packages/api/src/app.module.ts
+++ packages/api/src/app.module.ts
@@ -1,19 +1,23 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { BaseModule } from "@cryptify/api/src/base/base.module";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AuthenticationModule } from "./authentication/authentication.module";
 import { UsersModule } from "./users/users.module";
-import { dataSourceOptions } from "@cryptify/common/src/db/data_source_options";
+import { dataSourceOptionsConfig } from "@cryptify/common/src/db/data_source_options";
 
 @Module({
     imports: [
         ConfigModule.forRoot({
             envFilePath: `.env.${process.env.NODE_ENV}`,
+            isGlobal: true,
         }),
-        TypeOrmModule.forRoot({
-            ...dataSourceOptions,
-            synchronize: true,
+        TypeOrmModule.forRootAsync({
+            inject: [ConfigService],
+            useFactory: (config: ConfigService) => ({
+                ...dataSourceOptionsConfig(config),
+                synchronize: true,
+            }),
         }),
         BaseModule,
         AuthenticationModule,

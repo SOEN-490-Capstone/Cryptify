a383b702a95b093f9ee6709bea7366e3d9e7344a
Andre <psnmeee@gmail.com>
2022-10-02 14:09:02 -0400

    if you see this no you didnt

--- packages/api/src/users/users.service.ts
+++ packages/api/src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "@cryptify/common/src/entities/user";
-import { Repository } from "typeorm";
+import { InsertResult, Repository } from "typeorm";
 import { SignUpRequest } from "@cryptify/common/src/requests/sign_up_request";
 import { ERROR_EMAIL_IN_USE } from "@cryptify/common/src/errors/error_messages";
 
@@ -12,13 +12,13 @@ export class UsersService {
         private userRepository: Repository<User>,
     ) {}
 
-    async create(signUpReq: SignUpRequest): Promise<User> {
+    async create(signUpReq: SignUpRequest): Promise<InsertResult> {
         if (await this.findOne(signUpReq.email)) {
             throw new BadRequestException(ERROR_EMAIL_IN_USE);
         }
 
         const createdUser = this.userRepository.create(signUpReq);
-        return this.userRepository.save(createdUser);
+        return this.userRepository.insert(createdUser);
     }
 
     async findOne(email: string): Promise<User> {

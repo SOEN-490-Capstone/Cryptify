31ccc15b19935cd8930539bdf7dfd6506e00211b
Jason Gerard <jasongerard321@gmail.com>
2022-10-02 02:31:28 -0400

    Re-did ERD in plantuml

--- packages/diagrams/generate_diagrams.ts
+++ packages/diagrams/generate_diagrams.ts
@@ -2,14 +2,22 @@ const dir = ".";
 import * as fs from "fs";
 import { exec } from "node:child_process";
 
+console.log("Diagram generation starting...");
+
 fs.readdirSync(dir).forEach((file) => {
     if (file.endsWith(".puml")) {
-        exec(`puml generate ${file} -o ${file.split(".")[0]}.png -c monochrome`, (error, stdout, stderr) => {
+        exec(`puml generate ${file} -o generated/${file.split(".")[0]}.png -c monochrome`, (error, stdout, stderr) => {
             if (error) {
                 console.log(`Error: ${error}`);
             }
-            console.log(`stdout: ${stdout}`);
-            console.log(`stderr: ${stderr}`);
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+            }
+            if (stdout) {
+                console.log(`stdout: ${stdout}`);
+            }
         });
     }
 });
+
+console.log("Diagram generation done...");

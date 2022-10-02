5ecad76ff97718196e27cdbeab4ffa6938c16f97
Andre <psnmeee@gmail.com>
2022-10-02 15:33:20 -0400

    added integration test

---
+++ packages/api/test/integration/wallets_api.spec.ts
@@ -0,0 +1,36 @@
+import { agent } from "supertest";
+import { Test } from "@nestjs/testing";
+import { AppModule } from "../../src/app.module";
+import { INestApplication } from "@nestjs/common";
+
+describe("Wallets", () => {
+    let app: INestApplication;
+
+    beforeAll(async () => {
+        const moduleFixture = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    it("POST /wallets", async () => {
+        const token = (
+            await agent(app.getHttpServer()).post("/auth/signup").send({
+                firstName: "fname",
+                lastName: "lname",
+                email: "test@test.com",
+                password: "Test123!",
+                confirmPassword: "Test123!",
+            })
+        ).body.accessToken;
+
+        const res = await agent(app.getHttpServer())
+            .post("/wallets")
+            .set("Authorization", `bearer ${token}`)
+            .send({ address: "Test", name: "test" });
+
+        expect(res.status).toEqual(201);
+    });
+});

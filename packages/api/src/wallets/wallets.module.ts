f084a6d2706810a34f0d17b3b7d1618c3716e805
Andre <psnmeee@gmail.com>
2022-10-04 14:38:00 -0400

    lint fix

--- packages/api/src/wallets/wallets.module.ts
+++ packages/api/src/wallets/wallets.module.ts
@@ -4,8 +4,6 @@ import { WalletsService } from "@cryptify/api/src/wallets/wallets.service";
 import { WalletsController } from "@cryptify/api/src/wallets/wallets.controller";
 import { Wallet } from "@cryptify/common/src/entities/wallet";
 import { EthEdgeRequest } from "@cryptify/api/src/gateways/eth_edge_request";
-import { AuthorizationInterceptor } from "@cryptify/api/src/wallets/interceptors/authorization.interceptor";
-import { APP_INTERCEPTOR } from "@nestjs/core";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Wallet])],

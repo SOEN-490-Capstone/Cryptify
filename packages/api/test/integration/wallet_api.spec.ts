dfa38ffcd0a085f993a08d18377acb22506efb08
Andre <psnmeee@gmail.com>
2022-10-05 01:29:25 -0400

    added integration test

---
+++ packages/api/test/integration/wallet_api.spec.ts
@@ -0,0 +1,68 @@
+import { agent } from "supertest";
+import { Test } from "@nestjs/testing";
+import { AppModule } from "../../src/app.module";
+import { INestApplication } from "@nestjs/common";
+import { clearDB } from "@cryptify/common/src/db/clear_db";
+
+describe("Wallets", () => {
+    let app: INestApplication;
+    let resSignUp;
+
+    beforeAll(async () => {
+        const moduleFixture = await Test.createTestingModule({
+            imports: [AppModule],
+        }).compile();
+
+        app = moduleFixture.createNestApplication();
+        await app.init();
+    });
+
+    beforeEach(async () => {
+        await clearDB();
+
+        resSignUp = await agent(app.getHttpServer()).post("/auth/signup").send({
+            firstName: "fname",
+            lastName: "lname",
+            email: "test@test.com",
+            password: "Test123!",
+            confirmPassword: "Test123!",
+        });
+    });
+
+    describe("POST /user/:id/wallet", () => {
+        it("should return a wallet entity if post is successful", async () => {
+            console.log(process.env.NODE_ENV);
+
+            const res = await agent(app.getHttpServer())
+                .post("/user/1/wallet")
+                .set("Authorization", `Bearer ${resSignUp.body.accessToken}`)
+                .send({
+                    name: "test",
+                    address: "test",
+                    currencyType: "BITCOIN",
+                });
+
+            expect(res.status).toEqual(201);
+            expect(res.body).toEqual(
+                expect.objectContaining({
+                    name: expect.any(String),
+                    address: expect.any(String),
+                    currencyType: expect.any(String),
+                }),
+            );
+        });
+
+        it("should return a 400 status code if id in params isn't the current user", async () => {
+            const res = await agent(app.getHttpServer())
+                .post("/user/2/wallet")
+                .set("Authorization", `Bearer ${resSignUp.body.accessToken}`)
+                .send({
+                    name: "test",
+                    address: "test",
+                    currencyType: "BITCOIN",
+                });
+
+            expect(res.status).toEqual(401);
+        });
+    });
+});

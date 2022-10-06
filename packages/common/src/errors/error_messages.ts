773de612f947afbb7e151a7ec43a8553c42e840f
Andre <psnmeee@gmail.com>
2022-10-06 12:42:46 -0400

    err

--- packages/common/src/errors/error_messages.ts
+++ packages/common/src/errors/error_messages.ts
@@ -2,5 +2,5 @@ export const ERROR_EMAIL_OR_PASSWORD_INCORRECT = "Your email or password was inc
 export const ERROR_EMAIL_IN_USE = "This email address is already associated with another account.";
 export const ERROR_NOP = "nop";
 export const ERROR_WALLET_ADDRESS_FOR_CURRENCY = (currency: string) =>
-    `his wallet address is already associated with another ${currency} wallet.`;
+    `This wallet address is already associated with another ${currency} wallet.`;
 export const ERROR_WALLET_ALREADY_ADDED_TO_ACCOUNT = "This wallet address is already associated with another wallet.";

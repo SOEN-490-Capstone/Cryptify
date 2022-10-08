e8beac3ee3c0fdbe5fb71e9a221c8bf2185ebcdf
Andre <psnmeee@gmail.com>
2022-10-08 18:32:12 -0400

    added new users/:id/wallets endpoint

--- packages/api/src/wallets/guards/authorization.guard.ts
+++ packages/api/src/wallets/guards/authorization.guard.ts
@@ -4,10 +4,17 @@ import { Observable } from "rxjs";
 @Injectable()
 export class AuthorizationGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
-        const { body, params, user } = context.switchToHttp().getRequest();
+        const { route, body, params, user } = context.switchToHttp().getRequest();
 
-        //Makes sure the user id in the token, url params, and body are all the same.
-        if (params.id != user.id || params.id != body.userId) {
+        //Makes sure the user id in the token and url params are the same for GET or DELETE.
+        if (route.get || route.delete) {
+            if (params.id != user.id) {
+                throw new UnauthorizedException();
+            }
+        }
+        //Makes sure the user id in the token, url params, and body are all the same
+        //for request other than GET or DELETE.
+        else if (params.id != user.id || params.id != body.userId) {
             throw new UnauthorizedException();
         }
         return true;

2f62e9a5186be8e9d7d22b84349af78ab66772c4
Michael Warner <michael.gregory.warner@gmail.com>
2022-10-09 15:55:11 -0400

    Frontend: Added Wallets li to settings page & no wallets found to viewwallets page

--- packages/client/screens/SettingsScreen.tsx
+++ packages/client/screens/SettingsScreen.tsx
@@ -2,11 +2,43 @@ import React from "react";
 import { View } from "../components/Themed";
 import { StyleSheet } from "react-native";
 import SignOutButton from "../components/SignOutButton";
+import {Text, HStack, Pressable, FlatList, Box } from "native-base";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faChevronRightCustom } from "../components/icons/faChevronRightCustom";
+import {SettingsStackScreenProps } from "../types";
+import { faWalletCustom } from "../components/icons/faWalletCustom";
 
-export default function SettingsScreen() {
+export default function SettingsScreen({ navigation }: SettingsStackScreenProps<"ViewWalletsScreen">) {
+    const data = [
+        {
+            id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+            listItemTitle: "Wallets",
+        },
+    ];
     return (
         <View style={styles.view}>
             <SignOutButton />
+            <FlatList
+                data={data}
+                renderItem={({ item }) => (
+                    <Box >
+                        <Pressable
+                            onPress={() => navigation.navigate("ViewWalletsScreen")}
+                            style={styles.settingsListItem}
+                            _pressed={{
+                                background: "text.200",
+                            }}
+                        >
+                            <HStack height="50px" alignItems="center">
+                                <FontAwesomeIcon icon={faWalletCustom} style={styles.walletIcon} size={26} />
+                                <Text style={styles.settingsListText} >{item.listItemTitle}</Text>
+                                <FontAwesomeIcon icon={faChevronRightCustom} style={styles.chevronRightIcon} size={16} />
+                            </HStack>
+                        </Pressable>
+                    </Box>
+                )}
+                keyExtractor={(item) => item.id}
+            />
         </View>
     );
 }
@@ -16,5 +48,22 @@ const styles = StyleSheet.create({
         flex: 1,
         paddingHorizontal: 15,
         paddingTop: 10,
+    },
+    settingsListItem: {
+        paddingHorizontal: 10,
+        borderRadius: 10,
+    },
+    settingsListText: {
+        fontSize: 17,
+        lineHeight: 23,
+        marginLeft: 15,
+    },
+    walletIcon: {
+        color: "#404040",
+    },
+    chevronRightIcon: {
+        color: "#A3A3A3",
+        marginLeft: "auto",
+        marginRight: 5,
     },
 });
--- packages/client/screens/ViewWalletsScreen.tsx
+++ packages/client/screens/ViewWalletsScreen.tsx
@@ -1,6 +1,34 @@
 import React from "react";
 import { View } from "../components/Themed";
+import { Text, HStack, Pressable, FlatList, Box, VStack } from "native-base";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faWalletCustom } from "../components/icons/faWalletCustom";
+import { StyleSheet } from "react-native";
 
 export default function ViewWalletsScreen() {
-    return <View style={{ flex: 1, justifyContent: "center" }}></View>;
+    return (
+        <View style={styles.view}>
+            
+            {/* <Box>
+                <VStack height="94px" alignItems="center">
+                    <FontAwesomeIcon icon={faWalletCustom} style={styles.walletIcon} size={56} />
+                    <Text style={styles.settingsListText}>You do not have any wallets.</Text>
+                </VStack>
+            </Box> */}
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    view: {
+        flex: 1,
+        justifyContent: "center",
+    },
+    settingsListText: {
+        fontSize: 17,
+        marginTop: 15,
+    },
+    walletIcon: {
+        color: "#404040",
+    },
+});

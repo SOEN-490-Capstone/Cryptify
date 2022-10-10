3036ea08e39f4634f64397e0b2d8e24a57425f01
Jason Gerard <jasongerard321@gmail.com>
2022-10-10 15:44:54 -0400

    Add new address to webhook on create wallet

--- packages/eth-edge/src/gateways/alchemy_node.gateway.ts
+++ packages/eth-edge/src/gateways/alchemy_node.gateway.ts
@@ -1,26 +1,25 @@
-import { request, Method } from "@cryptify/common/src/helpers/request";
-import { CreateWalletRequest } from "@cryptify/common/src/requests/create_wallet_request";
+import { request, Method, RequestFunc } from "@cryptify/common/src/helpers/request";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
-import { WalletWithBalance } from "@cryptify/common/src/domain/wallet_with_balance";
-import { Wallet } from "@cryptify/common/src/domain/entities/wallet";
-import { GetWalletsRequest } from "@cryptify/common/src/requests/get_wallet_request";
 
 @Injectable()
 export class AlchemyNodeGateway {
-    // TODO fix this
-    request: <T>(method: Method, path: string, body: any) => Promise<T>;
+    request: RequestFunc;
     constructor(private configService: ConfigService) {
-        this.request = request(configService.get<string>("ETH_EDGE_HOST"), configService.get<string>("ETH_EDGE_PORT"));
+        const uri = `https://${configService.get<string>("ALCHEMY_HOST")}`;
+        this.request = request(uri);
     }
 
-    async createWallet(req: CreateWalletRequest): Promise<WalletWithBalance> {
-        const path = `user/${req.userId}/wallet`;
-        return this.request<WalletWithBalance>(Method.POST, path, req);
-    }
-
-    async getWallets(req: GetWalletsRequest): Promise<Wallet[]> {
-        const path = `users/${req.id}/wallets`;
-        return this.request<Wallet[]>(Method.GET, path, null);
+    async updateWebhookAddresses(addressesToAdd: string[], addressesToRemove: string[]): Promise<void> {
+        const path = "api/update-webhook-addresses";
+        const headers = {
+            "X-Alchemy-Token": this.configService.get<string>("ALCHEMY_TOKEN"),
+        };
+        const body = {
+            addresses_to_add: addressesToAdd,
+            addresses_to_remove: addressesToRemove,
+            webhook_id: this.configService.get<string>("ALCHEMY_ADDRESS_ACTIVITY_WEBHOOK_ID"),
+        };
+        return this.request(Method.PATCH, headers, path, body);
     }
 }
--- packages/eth-edge/src/modules/wallets.module.ts
+++ packages/eth-edge/src/modules/wallets.module.ts
@@ -5,10 +5,11 @@ import { Wallet } from "@cryptify/common/src/domain/entities/wallet";
 import { WalletsService } from "@cryptify/eth-edge/src/services/wallets.service";
 import { AlchemyNodeService } from "@cryptify/eth-edge/src/services/alchemy_node.service";
 import { TransactionsModule } from "./transactions.module";
+import { AlchemyNodeGateway } from "@cryptify/eth-edge/src/gateways/alchemy_node.gateway";
 
 @Module({
     imports: [TypeOrmModule.forFeature([Wallet]), TransactionsModule],
     controllers: [WalletsController],
-    providers: [WalletsService, AlchemyNodeService],
+    providers: [WalletsService, AlchemyNodeService, AlchemyNodeGateway],
 })
 export class WalletsModule {}

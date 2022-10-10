9a38ca113775de0a1e24a940d8a3721a31b1cf3a
Jason Gerard <jasongerard321@gmail.com>
2022-10-10 01:29:22 -0400

    Add gateway

---
+++ packages/eth-edge/src/gateways/alchemy_node.gateway.ts
@@ -0,0 +1,26 @@
+import { request, Method } from "@cryptify/common/src/helpers/request";
+import { CreateWalletRequest } from "@cryptify/common/src/requests/create_wallet_request";
+import { Injectable } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { WalletWithBalance } from "@cryptify/common/src/domain/wallet_with_balance";
+import { Wallet } from "@cryptify/common/src/domain/entities/wallet";
+import { GetWalletsRequest } from "@cryptify/common/src/requests/get_wallet_request";
+
+@Injectable()
+export class AlchemyNodeGateway {
+    // TODO fix this
+    request: <T>(method: Method, path: string, body: any) => Promise<T>;
+    constructor(private configService: ConfigService) {
+        this.request = request(configService.get<string>("ETH_EDGE_HOST"), configService.get<string>("ETH_EDGE_PORT"));
+    }
+
+    async createWallet(req: CreateWalletRequest): Promise<WalletWithBalance> {
+        const path = `user/${req.userId}/wallet`;
+        return this.request<WalletWithBalance>(Method.POST, path, req);
+    }
+
+    async getWallets(req: GetWalletsRequest): Promise<Wallet[]> {
+        const path = `users/${req.id}/wallets`;
+        return this.request<Wallet[]>(Method.GET, path, null);
+    }
+}

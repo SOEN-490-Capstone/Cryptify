1e70989496c62e7a676c3e91241bbf4bbc62786c
Jason Gerard <jasongerard321@gmail.com>
2022-10-15 13:58:11 -0400

    CRYP-000: add sprint 1 presentation (#51)
    * Refactor diagrams package to folder inside docs and added presentations
    
    * Update diagram gen script

---
+++ packages/docs/diagrams/generate_diagrams.ts
@@ -0,0 +1,25 @@
+const dir = "./diagrams";
+import * as fs from "fs";
+import { exec } from "node:child_process";
+
+console.log("Diagram generation starting...");
+
+fs.readdirSync(dir).forEach((file) => {
+    if (file.endsWith(".puml")) {
+        const inputFilePath = `${dir}/${file}`;
+        const outputFilePath = `${dir}/generated/${file.split(".")[0]}.png`;
+        exec(`puml generate ${inputFilePath} -o ${outputFilePath} -c monochrome`, (error, stdout, stderr) => {
+            if (error) {
+                console.log(`Error: ${error}`);
+            }
+            if (stderr) {
+                console.log(`stderr: ${stderr}`);
+            }
+            if (stdout) {
+                console.log(`stdout: ${stdout}`);
+            }
+        });
+    }
+});
+
+console.log("Diagram generation done...");

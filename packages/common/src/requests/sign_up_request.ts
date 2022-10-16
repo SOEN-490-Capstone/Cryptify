9968dbbf22e9f31164a3ef093782460e0932fce1
Jason Gerard <jasongerard321@gmail.com>
2022-10-16 17:18:57 -0400

    Fix lint

--- packages/common/src/requests/sign_up_request.ts
+++ packages/common/src/requests/sign_up_request.ts
@@ -1,4 +1,4 @@
-import {InferType} from "yup";
+import { InferType } from "yup";
 import { signUpSchema } from "@cryptify/common/src/validations/sign_up_schema";
 
-export interface SignUpRequest extends InferType<typeof signUpSchema> {}
+export type SignUpRequest = InferType<typeof signUpSchema>;

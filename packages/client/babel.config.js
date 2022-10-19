e922bb2068a187899d76e2c27a768eb2093f08ba
Jason Gerard <jasongerard321@gmail.com>
2022-10-19 17:18:40 -0400

    CRYP-20: Setup detox system testing framework (#53)
    * Setup detox and expo eas build
    
    * Fix expo building
    
    * Revert scripts
    
    * Added scripts and updated configs
    
    * Fix and refactor testing scripts and setup
    
    * Fix webpack watcher
    
    * Fix env files to not track local ones
    
    * Fix integration tests and refactor system test folder structure

--- packages/client/babel.config.js
+++ packages/client/babel.config.js
@@ -8,7 +8,7 @@ module.exports = function (api) {
                 {
                     envName: "APP_ENV",
                     moduleName: "@env",
-                    path: ".env",
+                    path: ".env.dev",
                 },
             ],
         ],
--- packages/client/webpack.config.js
+++ packages/client/webpack.config.js
@@ -7,6 +7,9 @@ module.exports = async function (env, argv) {
             babel: {
                 dangerouslyAddModulePathsToTranspile: ["@cryptify"],
             },
+            watchOptions: {
+                ignored: ["**/pgdata", "**/node_modules"],
+            },
         },
         argv,
     );

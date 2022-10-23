9d222ff39c678da647f996096e828e9c875fa823
Jason Gerard <jasongerard321@gmail.com>
2022-10-22 22:40:42 -0400

    Implement base abstract gateway

--- packages/api/src/gateways/eth_edge_gateway.ts
+++ packages/api/src/gateways/eth_edge_gateway.ts
@@ -1,18 +1,20 @@
+import fetch from "node-fetch";
 import { request, Method, RequestFunc } from "@cryptify/common/src/helpers/request";
 import { CreateWalletRequest } from "@cryptify/common/src/requests/create_wallet_request";
-import { Injectable } from "@nestjs/common";
+import {BadRequestException, Injectable} from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { WalletWithBalance } from "@cryptify/common/src/domain/wallet_with_balance";
 import { GetWalletsRequest } from "@cryptify/common/src/requests/get_wallet_request";
+import {AbstractGateway} from "@cryptify/common/src/abstract-gateway/abstract_gateway";
+import {HttpError} from "@cryptify/common/src/errors/http_error";
 
 @Injectable()
-export class EthEdgeGateway {
-    request: RequestFunc;
+export class EthEdgeGateway extends AbstractGateway {
 
     constructor(private configService: ConfigService) {
         const host = configService.get<string>("ETH_EDGE_HOST");
         const port = configService.get<string>("ETH_EDGE_PORT");
-        this.request = request(`http://${host}:${port}`);
+        super(`http://${host}:${port}`, fetch);
     }
 
     async createWallet(req: CreateWalletRequest): Promise<WalletWithBalance> {
@@ -23,5 +25,15 @@ export class EthEdgeGateway {
     async getWallets(req: GetWalletsRequest): Promise<WalletWithBalance[]> {
         const path = `users/${req.id}/wallets`;
         return this.request<WalletWithBalance[]>(Method.GET, {}, path, null);
+    }
+
+    protected async handleError(response: any): Promise<void> {
+        const resBody = await response.json();
+
+        if (response.status == 400) {
+            throw new BadRequestException(resBody.message);
+        }
+
+        throw new HttpError(resBody.message, response.status);
     }
 }
---
+++ packages/common/src/abstract-gateway/abstract_gateway.ts
@@ -0,0 +1,39 @@
+import {BadRequestException} from "@nestjs/common";
+import {HttpError} from "@cryptify/common/src/errors/http_error";
+
+export abstract class AbstractGateway {
+    protected constructor(private readonly uri: string, private readonly fetch: any) {}
+
+    async request<T>(method: Method, headers: Headers, path: string, body: any): Promise<T> {
+        const response = await this.fetch(`${this.uri}/${path}`, {
+            method: Method[method],
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+                ...headers,
+            },
+            body: body ? JSON.stringify(body) : null,
+        });
+
+        if (response.status >= 300) {
+            await this.handleError(response);
+        }
+
+        const resBody = await response.json();
+        return resBody as T;
+    }
+
+    protected abstract handleError(response: any): Promise<void>;
+}
+
+enum Method {
+    POST,
+    GET,
+    PUT,
+    PATCH,
+    DELETE,
+}
+
+type Headers = {
+    [key: string]: string;
+};

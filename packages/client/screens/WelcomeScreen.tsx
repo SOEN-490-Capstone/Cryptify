67fb74a25dd2611454c44849aebd8b1f6e6815c1
Jason Gerard <jasongerard321@gmail.com>
2022-10-27 17:00:28 -0400

    CRYP-100: welcome screen system test (#61)

--- packages/client/screens/WelcomeScreen.tsx
+++ packages/client/screens/WelcomeScreen.tsx
@@ -1,7 +1,7 @@
 import { View } from "../components/Themed";
 import React from "react";
 import { StyleSheet } from "react-native";
-import { Box, Button, Text, VStack } from "native-base";
+import { Button, Text, VStack } from "native-base";
 import { GuestStackScreenProps } from "../types";
 import { Logo } from "../components/icons/Logo";
 import { WelcomeScreenBackground } from "../components/icons/WelcomeScreenBackground";
@@ -9,16 +9,15 @@ import { WelcomeScreenBackground } from "../components/icons/WelcomeScreenBackgr
 export default function WelcomeScreen({ navigation }: GuestStackScreenProps<"WelcomeScreen">) {
     return (
         <View style={styles.view}>
-            <Box safeArea></Box>
-            <Box style={styles.titleContainer}>
-                <VStack space="20px">
-                    <Logo />
-                    <Text style={styles.subtext}>Crypto asset management{"\n"}made simple</Text>
-                </VStack>
-            </Box>
+            <VStack safeArea space="20px" style={styles.titleContainer}>
+                <Logo testID="logo" />
+                <Text style={styles.subtext}>Crypto asset management{"\n"}made simple</Text>
+            </VStack>
             <VStack space="15px" marginBottom="20px">
-                <Button onPress={() => navigation.navigate("SignUpScreen")}>Sign up</Button>
-                <Button variant="outline" onPress={() => navigation.navigate("SignInScreen")}>
+                <Button onPress={() => navigation.navigate("SignUpScreen")} testID="signUpButton">
+                    Sign up
+                </Button>
+                <Button variant="outline" onPress={() => navigation.navigate("SignInScreen")} testID="signInButton">
                     Sign in
                 </Button>
             </VStack>
--- packages/client/tests/system/sign_up.e2e.ts
+++ packages/client/tests/system/sign_up.e2e.ts
@@ -1,4 +1,4 @@
-const { openAppForDebugBuild } = require("./open_app_for_debug_build"); // eslint-disable-line @typescript-eslint/no-var-requires
+import { openAppForDebugBuild } from "./utils/open_app_for_debug_build";
 import { by, device, expect, element } from "detox";
 
 describe("Sign Up CRYP-21", () => {
@@ -10,7 +10,9 @@ describe("Sign Up CRYP-21", () => {
     });
 
     it("should have no errors in the signup flow. create user account, and sign in the user", async () => {
-        await element(by.id("Sign Up Tab")).tap();
+        await element(by.id("signUpButton")).tap();
+
+        await expect(element(by.text("Create an account"))).toBeVisible();
 
         await element(by.id("firstName")).typeText("John");
         await element(by.id("lastName")).typeText("Doe");

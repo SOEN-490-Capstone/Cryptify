6c53b37c66f6d2df5a4df48916236a151009bf64
Pola Farid <polacris2@gmail.com>
2022-10-27 19:54:56 -0400

    Modified sign in test

--- packages/client/tests/system/sign_in.e2e.ts
+++ packages/client/tests/system/sign_in.e2e.ts
@@ -1,7 +1,7 @@
 const { openAppForDebugBuild } = require("./open_app_for_debug_build"); // eslint-disable-line @typescript-eslint/no-var-requires
 import { by, device, expect, element } from "detox";
 
-describe("Sign Up CRYP-21", () => {
+describe("Sign In CRYP-22", () => {
     beforeEach(async () => {
         await device.launchApp({
             newInstance: true,
@@ -9,15 +9,12 @@ describe("Sign Up CRYP-21", () => {
         await openAppForDebugBuild();
     });
 
-    it("should have no errors in the signup flow. create user account, and sign in the user", async () => {
-        await element(by.id("Sign Up Tab")).tap();
+    it("should sign in the user to their account without any errors", async () => {
+        await element(by.id("Sign In Tab")).tap();
 
-        await element(by.id("firstName")).typeText("John");
-        await element(by.id("lastName")).typeText("Doe");
         await element(by.id("email")).typeText("john@example.com");
         await element(by.id("password")).typeText("Test123!");
-        await element(by.id("confirmPassword")).typeText("Test123!");
-        await element(by.text("Sign up")).tap();
+        await element(by.text("Sign In")).tap();
 
         await expect(element(by.id("token"))).toBeVisible();
     });

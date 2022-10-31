c6ae2a84efa14f41e49e64060fdc983b6c0f7ee1
Jason Gerard <jasongerard321@gmail.com>
2022-10-31 18:16:09 -0400

    Finish config with eth-edge

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,13 +1,13 @@
 const scanner = require('sonarqube-scanner');
 scanner({
     serverUrl: "http://localhost:9000",
-    token: "squ_574c61649698b70c012e8e5ddb2b2883e8663f5e",
+    token: "squ_352fcb00d78326b7379bd3f713e4c84997fa0929",
     options: {
         "sonar.sources": "./packages/api/src, ./packages/eth-edge/src",
         "sonar.tests": "./packages/api/test, ./packages/eth-edge/test",
         "sonar.test.inclusions": "**/*.spec.ts",
         "sonar.typescript.lcov.reportPaths": "**/coverage/lcov.info",
         "sonar.exclusions": "**/android/**/*, **/ios/**/*, **/test/**/*, **/tests/**/*",
-        "sonar.testExecutionReportPaths": ".packages/api/test-report.xml"
+        "sonar.testExecutionReportPaths": "./packages/api/test-report.xml, ./packages/eth-edge/test-report.xml"
     },
 }, () => process.exit());

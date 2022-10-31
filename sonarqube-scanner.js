7cc6642eb9f1b16971fd1e9588d8c647dfc0adff
Jason Gerard <jasongerard321@gmail.com>
2022-10-31 14:27:19 -0400

    Add test to sonarqube

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -3,7 +3,11 @@ scanner({
     serverUrl: "http://localhost:9000",
     token: "squ_574c61649698b70c012e8e5ddb2b2883e8663f5e",
     options: {
-        "sonar.sources": "./packages/api, ./packages/eth-edge, ./packages/client",
+        "sonar.sources": "./packages/api/src, ./packages/eth-edge/src",
+        "sonar.tests": "./packages/api/test, ./packages/eth-edge/test",
+        "sonar.test.inclusions": "**/*.spec.ts",
+        "sonar.typescript.lcov.reportPaths": "**/coverage/lcov.info",
         "sonar.exclusions": "**/android/**/*, **/ios/**/*, **/test/**/*, **/tests/**/*",
+        "sonar.testExecutionReportPaths": ".packages/api/test-report.xml"
     },
 }, () => process.exit());

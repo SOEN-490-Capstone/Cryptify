6760aa7658a0ead17ab7dc6b45c548b4788e4cf6
Domenic Seccareccia <dom_seccareccia@hotmail.com>
2022-11-01 20:23:39 -0400

    CRYP-183: Implement UI mockup typography (#72)

---
+++ packages/client/theme/base/typography.tsx
@@ -0,0 +1,48 @@
+export const typography = {
+    lineHeights: {
+        caption2: 15,
+        caption1: 16,
+        footnote2: 17,
+        footnote1: 19,
+        subheadline: 20,
+        callout: 21,
+        body: 23,
+        headline: 23,
+        title3: 27,
+        title2: 29,
+        title1: 37,
+        largeTitle: 45,
+    },
+    fontConfig: {
+        SegoeUI: {
+            400: {
+                normal: "SegoeUI-Regular",
+            },
+            600: {
+                normal: "SegoeUI-Semibold",
+            },
+            700: {
+                normal: "SegoeUI-Bold",
+            },
+        },
+    },
+    fonts: {
+        heading: "SegoeUI",
+        body: "SegoeUI",
+        mono: "SegoeUI",
+    },
+    fontSizes: {
+        caption2: 11,
+        caption1: 12,
+        footnote2: 13,
+        footnote1: 14,
+        subheadline: 15,
+        callout: 16,
+        body: 17,
+        headline: 17,
+        title3: 20,
+        title2: 22,
+        title1: 28,
+        largeTitle: 34,
+    },
+};
--- packages/client/theme/extendTheme.tsx
+++ packages/client/theme/extendTheme.tsx
@@ -1,4 +1,5 @@
 import { extendTheme } from "native-base";
+import { typography } from "./base/typography";
 import { text } from "./components/text";
 import { button } from "./components/button";
 import { input } from "./components/input";
@@ -6,6 +7,7 @@ import { formControlErrorMessage } from "./components/form-control";
 
 export default function () {
     return extendTheme({
+        ...typography,
         components: {
             Text: text,
             Button: button,

835be7f3a2d26151d1a19ec91a7368771f65492a
Jason Gerard <jasongerard321@gmail.com>
2022-11-04 16:02:45 -0400

    Update class diagrams

--- packages/common/src/requests/create_wallet_request.ts
+++ packages/common/src/requests/create_wallet_request.ts
@@ -1,4 +1,4 @@
 import { InferType } from "yup";
 import { createWalletSchema } from "@cryptify/common/src/validations/create_wallet_schema";
 
-export type CreateWalletRequest = InferType<typeof createWalletSchema>;
+export interface CreateWalletRequest extends InferType<typeof createWalletSchema> {}
--- packages/common/src/requests/get_transaction_request.ts
+++ packages/common/src/requests/get_transaction_request.ts
@@ -1,4 +1,4 @@
 import { InferType } from "yup";
 import { getTransactionsSchema } from "../validations/get_transactions_schema";
 
-export type GetTransactionsRequest = InferType<typeof getTransactionsSchema>;
+export interface GetTransactionsRequest extends InferType<typeof getTransactionsSchema> {}

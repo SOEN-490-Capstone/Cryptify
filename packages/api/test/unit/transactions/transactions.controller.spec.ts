258c1ff127ad2740329b0d3b7170906a60945096
Andre <psnmeee@gmail.com>
2022-11-04 19:07:55 -0400

    added api unit test

---
+++ packages/api/test/unit/transactions/transactions.controller.spec.ts
@@ -0,0 +1,41 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { TransactionsController } from "../../../src/controllers/transactions.controller";
+import { TransactionsService } from "../../../src/services/transactions.service";
+
+describe("TransactionController", () => {
+    let controller: TransactionsController;
+    let fakeTransactionService: Partial<TransactionsService>;
+
+    const transaction = {
+        transactionAddress: "string",
+        walletIn: "string",
+        walletOut: "string",
+        amount: "string",
+        createdAt: new Date(),
+    };
+
+    beforeEach(async () => {
+        fakeTransactionService = {
+            findAll: async ({id: number}) => {
+                return [transaction];
+            },
+        };
+
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [TransactionsController],
+            providers: [
+                { provide: TransactionsService, useValue: fakeTransactionService },
+            ],
+        }).compile();
+
+        controller = module.get<TransactionsController>(TransactionsController);
+    });
+
+    it("should return list of transactions", async () => {
+        const req = {
+                id: 1,
+            };
+
+        expect(await controller.findAll(req)).toEqual([transaction]);
+    });
+});

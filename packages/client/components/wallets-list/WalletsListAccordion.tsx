72c46e9543a8b8fc4aca6d1e37c93c9cfc7aad8b
Domenic Seccareccia <dom_seccareccia@hotmail.com>
2022-11-09 17:16:58 -0500

    Add regular chevron down icon

--- packages/client/components/wallets-list/WalletsListAccordion.tsx
+++ packages/client/components/wallets-list/WalletsListAccordion.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Pressable, Box, HStack, Text, VStack } from "native-base";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faChevronRight } from "../icons/regular/faChevronRight";
-import { faChevronDownCustom } from "../icons/faChevronDownCustom";
+import { faChevronDown } from "../icons/regular/faChevronDown";
 import { StyleSheet } from "react-native";
 import Accordion from "react-native-collapsible/Accordion";
 import { WalletWithBalance } from "@cryptify/common/src/domain/wallet_with_balance";
@@ -62,7 +62,7 @@ export function WalletsListAccordion({ wallets, showCurrencyTotals, navigation }
                     <Box flex={1}></Box>
                 )}
                 <FontAwesomeIcon
-                    icon={isActive ? faChevronDownCustom : faChevronRight}
+                    icon={isActive ? faChevronDown : faChevronRight}
                     style={styles.chevronIcon}
                     size={16}
                 />

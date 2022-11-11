f16d07de1005f36ff8e2d429b02b3d632bce40b5
Jason Gerard <jasongerard321@gmail.com>
2022-11-11 17:37:43 -0500

    Add basic websocket module

--- packages/btc-edge/src/modules/transactions.module.ts
+++ packages/btc-edge/src/modules/transactions.module.ts
@@ -3,10 +3,17 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { Transaction } from "@cryptify/common/src/domain/entities/transaction";
 import { TransactionsService } from "../services/transactions_service";
 import { SoChainGateway } from "../gateways/so_chain_gateway";
+import {BlockchainWebsocketService} from "@cryptify/btc-edge/src/gateways/blockchain_websocket";
+import {WebSocketModule} from "nestjs-websocket";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Transaction])],
-    providers: [TransactionsService, SoChainGateway],
+    imports: [
+        TypeOrmModule.forFeature([Transaction]),
+        WebSocketModule.forRoot({
+            url: "wss://ws.blockchain.info/inv",
+        }),
+    ],
+    providers: [TransactionsService, SoChainGateway, BlockchainWebsocketService],
     exports: [TransactionsService],
 })
 export class TransactionsModule {}

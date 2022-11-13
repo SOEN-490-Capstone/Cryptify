cb35e33d337f2dcbcd2eb65c8934cbbff3a5ec8b
Jason Gerard <jasongerard321@gmail.com>
2022-11-13 12:58:44 -0500

    Update currency utils func and add unit tests

--- packages/common/src/utils/currency_utils.ts
+++ packages/common/src/utils/currency_utils.ts
@@ -1,11 +1,13 @@
-import { CurrencyType } from "@cryptify/common/src/domain/currency_type";
+import {CurrencyType} from "@cryptify/common/src/domain/currency_type";
+import {validate} from "wallet-address-validator";
 
 export function getCurrencyType(address: string): CurrencyType {
-    // TODO these regex are not good at all and should be refactored
-    // to either a formal implementation in the system or using
-    // a well known package
-    if (address.includes("0x")) return CurrencyType.ETHEREUM;
-    if (/^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$/.test(address)) return CurrencyType.BITCOIN;
+    if (validate(address, 'BTC')) {
+        return CurrencyType.BITCOIN
+    }
+    if (validate(address, 'ETH')) {
+        return CurrencyType.ETHEREUM
+    }
 
     throw new Error("Currency type not supported");
 }

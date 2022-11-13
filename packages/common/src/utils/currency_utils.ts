b6fb1e24a800cf618f03eb9d264da408605188f1
Jason Gerard <jasongerard321@gmail.com>
2022-11-13 13:41:09 -0500

    Fix error in util func

--- packages/common/src/utils/currency_utils.ts
+++ packages/common/src/utils/currency_utils.ts
@@ -1,14 +1,18 @@
 import { CurrencyType } from "@cryptify/common/src/domain/currency_type";
 import { validate } from "wallet-address-validator";
 
+const btcTxRegex = /^[a-fA-F0-9]{64}$/;
+const ethTxRegex = /^0x([A-Fa-f0-9]{64})$/;
+
 export function getCurrencyType(address: string): CurrencyType {
-    if (validate(address, "BTC")) {
+    if (validate(address, "BTC") || btcTxRegex.test(address)) {
         return CurrencyType.BITCOIN;
     }
-    if (validate(address, "ETH")) {
+    if (validate(address, "ETH") || ethTxRegex.test(address)) {
         return CurrencyType.ETHEREUM;
     }
 
+    console.log(address.length);
     throw new Error("Currency type not supported");
 }
 

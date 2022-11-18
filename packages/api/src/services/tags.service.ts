396af8e6a238179acf1760fe6302beb4bb3dea7c
Alexandru Bara <alexandrubara2000@gmail.com>
2022-11-18 13:11:53 -0500

    Added api for adding a tag

--- packages/api/src/services/tags.service.ts
+++ packages/api/src/services/tags.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { TransactionTag } from "@cryptify/common/src/domain/entities/TransactionTag";
+import { CreateTagRequest } from "@cryptify/common/src/requests/create_tag_request";
+import { ERROR_TAG_NAME_ALREADY_ADDED_TO_ACCOUNT } from "@cryptify/common/src/errors/error_messages";
 
 @Injectable()
 export class TagsService {
@@ -12,5 +14,14 @@ export class TagsService {
 
     async findAll(userId: number): Promise<TransactionTag[]> {
         return this.tagRepository.findBy({ userId });
+    }
+
+    async create(req: CreateTagRequest): Promise<TransactionTag> {
+        if (await this.tagRepository.findOneBy(req)) {
+            throw new BadRequestException(ERROR_TAG_NAME_ALREADY_ADDED_TO_ACCOUNT);
+        }
+        const createdTag = this.tagRepository.create(req); 
+        await this.tagRepository.insert(createdTag);
+        return await this.tagRepository.findOneBy(createdTag);
     }
 }
---
+++ packages/common/src/validations/create_tag_schema.ts
@@ -0,0 +1,7 @@
+import * as yup from "yup";
+
+
+export const createTagSchema = yup.object({
+    userId: yup.number().required(),
+    tagName: yup.string().required(),
+});

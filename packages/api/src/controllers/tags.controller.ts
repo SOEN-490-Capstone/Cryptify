3be04a62331032dfd4796250ad3086a87f64309e
Alexandru Bara <60298505+alexbara2000@users.noreply.github.com>
2022-11-19 19:28:28 -0500

    CRYP-35: Created API for create a transaction (#115)

--- packages/api/src/controllers/tags.controller.ts
+++ packages/api/src/controllers/tags.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, Param, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, UseGuards } from "@nestjs/common";
 import { useValidate } from "@cryptify/common/src/hooks/use_validate";
 import { JwtAuthGuard } from "@cryptify/api/src/guards/jwt-auth.guard";
 import { CanAccessResourceGuard } from "@cryptify/api/src/guards/can_access_resource.guard";
 import { getTagsSchema } from "@cryptify/common/src/validations/get_tags_schema";
 import { GetTagsRequest } from "@cryptify/common/src/requests/get_tags_request";
+import { createTagSchema } from "@cryptify/common/src/validations/create_tag_schema";
+import { CreateTagRequest } from "@cryptify/common/src/requests/create_tag_request";
 import { TagsService } from "../services/tags.service";
 import { TransactionTag } from "@cryptify/common/src/domain/entities/TransactionTag";
+import { CanMutateResourceGuard } from "../guards/can_mutate_resource.guard";
 
 @Controller()
 export class TagsController {
@@ -16,5 +19,12 @@ export class TagsController {
     async get(@Param() params: GetTagsRequest): Promise<TransactionTag[]> {
         const getTagsReq = await useValidate(getTagsSchema, params);
         return this.tagsService.findAll(getTagsReq.id);
+    }
+
+    @UseGuards(JwtAuthGuard, CanMutateResourceGuard)
+    @Post("/users/:id/tags")
+    async create(@Body() body: CreateTagRequest): Promise<TransactionTag> {
+        const createTagReq = await useValidate(createTagSchema, body);
+        return this.tagsService.create(createTagReq);
     }
 }

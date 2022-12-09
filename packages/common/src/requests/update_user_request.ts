b9d0efe58e04d86a8e56625a42c29e587b6e0bf1
Andre <psnmeee@gmail.com>
2022-12-09 00:23:30 -0500

    changed type to interface

--- packages/common/src/requests/update_user_request.ts
+++ packages/common/src/requests/update_user_request.ts
@@ -1,8 +1,8 @@
-export type UpdateUserRequest = {
+export interface UpdateUserRequest {
     userId: number;
     firstName?: string;
     lastName?: string;
     email?: string;
     password?: string;
     areNotificationsEnabled?: boolean;
-};
+}

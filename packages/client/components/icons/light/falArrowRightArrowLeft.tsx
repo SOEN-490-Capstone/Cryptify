3e094dc484a17405024dc2ad9e76dbf832c1e040
Andre <psnmeee@gmail.com>
2022-12-10 19:27:19 -0500

    fix icon names

--- packages/client/components/icons/light/falArrowRightArrowLeft.tsx
+++ packages/client/components/icons/light/falArrowRightArrowLeft.tsx
@@ -13,6 +13,6 @@ export const falArrowRightArrowLeft: IconDefinition = {
         // SVG path data
         "M16 160h441.4l-100.7 100.7c-6.25 6.25-6.25 16.38 0 22.62s16.38 6.25 22.62 0l128-128C510.4 152.2 512 148.1 512 144s-1.562-8.188-4.688-11.31l-128-128c-6.25-6.25-16.38-6.25-22.62 0s-6.25 16.38 0 22.62L457.4 128H16C7.156 128 0 135.2 0 144S7.156 160 16 160zM496 352H54.63l100.7-100.7c6.25-6.25 6.25-16.38 0-22.62s-16.38-6.25-22.62 0l-128 128C1.563 359.8 0 363.9 0 368s1.562 8.188 4.688 11.31l128 128c6.25 6.25 16.38 6.25 22.62 0s6.25-16.38 0-22.62L54.63 384H496c8.844 0 16-7.156 16-16S504.8 352 496 352z",
     ],
-    iconName: "fac-chevron-down" as IconName,
-    prefix: "fac-regular" as IconPrefix,
+    iconName: "fac-arrow-right-arrow-left" as IconName,
+    prefix: "fac-light" as IconPrefix,
 };

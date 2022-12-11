6dbfe99fbb78195b6b5641df033d37332fc80e0f
Andre <psnmeee@gmail.com>
2022-12-10 19:24:03 -0500

    pr comments

--- packages/client/screens/NotificationsScreen.tsx
+++ packages/client/screens/NotificationsScreen.tsx
@@ -4,11 +4,11 @@ import { StyleSheet, Switch } from "react-native";
 import { AuthContext } from "../components/contexts/AuthContext";
 import { UsersGateway } from "../gateways/users_gateway";
 import { HStack, VStack, Text } from "native-base";
-import { farArrowRightArrowLeft } from "../components/icons/regular/farArrowRightArrowLeft";
+import { falArrowRightArrowLeft } from "../components/icons/light/falArrowRightArrowLeft";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 
 export default function NotificationsScreen() {
-    const { token, user } = React.useContext(AuthContext);
+    const { token, user, setUser } = React.useContext(AuthContext);
     const usersGateway = new UsersGateway();
 
     const [isEnabled, setIsEnabled] = React.useState(user.areNotificationsEnabled);
@@ -22,23 +22,24 @@ export default function NotificationsScreen() {
         );
 
         user.areNotificationsEnabled = !isEnabled;
+        setUser(user);
+
         setIsEnabled(!isEnabled);
     }
 
     return (
         <View style={styles.view}>
-            <HStack alignItems={"center"} space={19}>
-                <FontAwesomeIcon icon={farArrowRightArrowLeft} size={26} />
-                <VStack style={{ maxWidth: 280, paddingRight: 75 }}>
+            <HStack alignItems={"center"} space={"lg"}>
+                <FontAwesomeIcon icon={falArrowRightArrowLeft} size={26} />
+                <VStack>
                     <Text>Trasactions</Text>
                     <Text color="text.500" size={"footnote1"}>
                         Send and receive transactions for all wallets
                     </Text>
                 </VStack>
                 <Switch
-                    trackColor={{ false: "#767577", true: "#81b0ff" }}
-                    thumbColor={"#f4f3f4"}
-                    ios_backgroundColor="#3e3e3e"
+                    trackColor={{ false: "#787880", true: "#0077E6" }}
+                    thumbColor={"#FFFFFF"}
                     onValueChange={toggleSwitch}
                     value={isEnabled}
                 />
@@ -51,6 +52,6 @@ const styles = StyleSheet.create({
     view: {
         flex: 1,
         paddingHorizontal: 15,
-        paddingTop: 40,
+        paddingTop: 30,
     },
 });

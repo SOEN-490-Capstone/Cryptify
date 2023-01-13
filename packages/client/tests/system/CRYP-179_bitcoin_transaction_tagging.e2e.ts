ac813a51261f1b2e55a6d54f0daed6563ba22b59
Jason Gerard <jasongerard321@gmail.com>
2023-01-13 17:51:15 -0500

    Add system test for CRYP-179

--- packages/client/tests/system/CRYP-179_bitcoin_transaction_tagging.e2e.ts
+++ packages/client/tests/system/CRYP-179_bitcoin_transaction_tagging.e2e.ts
@@ -2,7 +2,7 @@ import { openAppForDebugBuild } from "./utils/open_app_for_debug_build";
 import { by, device, expect, element } from "detox";
 import { pause, pressBackLeft } from "./utils/test_utils";
 
-describe("CRYP-33 Ethereum transaction tagging", () => {
+describe("CRYP-179 Bitcoin transaction tagging", () => {
     beforeEach(async () => {
         await device.launchApp({
             newInstance: true,
@@ -10,7 +10,7 @@ describe("CRYP-33 Ethereum transaction tagging", () => {
         await openAppForDebugBuild();
     });
 
-    it("Should be able to tag an Ethereum transaction with a tag", async () => {
+    it("Should be able to tag an Bitcoin transaction with a tag", async () => {
         // Sign in
         await element(by.id("signInButton")).tap();
         await expect(element(by.text("Welcome back"))).toBeVisible();
@@ -18,10 +18,10 @@ describe("CRYP-33 Ethereum transaction tagging", () => {
         await element(by.id("password")).typeText("Test123!");
         await element(by.text("Sign in")).tap();
 
-        // Nav to an Ethereum transaction
-        await element(by.id("walletsListETHEREUM")).tap();
-        await expect(element(by.id("walletsListItemETHEREUM")).atIndex(0)).toBeVisible();
-        await element(by.id("walletsListItemETHEREUM")).atIndex(0).tap();
+        // Nav to an Bitcoin transaction
+        await element(by.id("walletsListBITCOIN")).tap();
+        await expect(element(by.id("walletsListItemBITCOIN")).atIndex(0)).toBeVisible();
+        await element(by.id("walletsListItemBITCOIN")).atIndex(0).tap();
         await element(by.id("transactionsListItem")).atIndex(0).tap();
 
         // Add existing tag to transaction
@@ -30,7 +30,7 @@ describe("CRYP-33 Ethereum transaction tagging", () => {
 
         // Add new tag to transaction
         await element(by.id("addNewTagButton")).tap();
-        await element(by.id("newTagInput")).typeText("Ethereum tag\n");
+        await element(by.id("newTagInput")).typeText("Bitcoin tag\n");
 
         await pressBackLeft();
 

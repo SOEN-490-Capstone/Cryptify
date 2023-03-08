660fbd88946dad8d18b6d7d9c2f5f03f4db8f29c
Jason Gerard <jasongerard321@gmail.com>
2023-03-08 11:21:43 -0500

    Add wallet builder pattern

--- packages/common/src/domain/entities/wallet.ts
+++ packages/common/src/domain/entities/wallet.ts
@@ -2,6 +2,8 @@ import { Column, Entity, JoinColumn, ManyToOne, OneToMany, PrimaryColumn } from 
 import { User } from "@cryptify/common/src/domain/entities/user";
 import { Transaction } from "./transaction";
 import { CurrencyType } from "@cryptify/common/src/domain/currency_type";
+import {getCurrencyType} from "@cryptify/common/src/utils/currency_utils";
+import {WalletWithBalance} from "@cryptify/common/src/domain/wallet_with_balance";
 
 @Entity()
 export class Wallet {
@@ -16,11 +18,71 @@ export class Wallet {
 
     @ManyToOne(() => User, (user) => user.wallets)
     @JoinColumn({ name: "userId", referencedColumnName: "id" })
-    user: User;
+    user?: User;
 
     @Column({ type: "enum", enum: CurrencyType })
     currencyType: CurrencyType;
 
     @OneToMany(() => Transaction, (transaction) => transaction.walletIn || transaction.walletOut)
     transactions: Transaction[];
+}
+
+export class WalletBuilder {
+    private address: string;
+    private userId: number;
+    private name: string;
+    private user: User;
+    private transactions: Transaction[] = [];
+    private balance: string;
+
+    setAddress(address: string): this {
+        this.address = address;
+        return this;
+    }
+
+    setUserId(userId: number): this {
+        this.userId = userId;
+        return this;
+    }
+
+    setName(name: string): this {
+        this.name = name;
+        return this;
+    }
+
+    setUser(user: User): this {
+        this.user = user;
+        return this;
+    }
+
+    setTransactions(transactions: Transaction[]): this {
+        this.transactions.push(...transactions);
+        return this;
+    }
+    
+    setWallet(wallet: Wallet): this {
+        this.address = wallet.address;
+        this.userId = wallet.userId;
+        this.name = wallet.name;
+        return this;
+    }
+
+    setBalance(balance: string): this {
+        this.balance = balance;
+        return this;
+    }
+
+    build(): WalletWithBalance {
+        const currencyType = getCurrencyType(this.address);
+        
+        return {
+            address: this.address,
+            userId: this.userId,
+            name: this.name,
+            user: this.user,
+            currencyType,
+            transactions: this.transactions,
+            balance: this.balance,
+        };
+    }
 }

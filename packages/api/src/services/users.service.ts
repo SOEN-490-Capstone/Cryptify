c4f3d68cffe7ac2af43f6d83a66be1191f7b5cfa
Andre <psnmeee@gmail.com>
2023-03-08 23:39:23 -0500

    lint fix

--- packages/api/src/services/users.service.ts
+++ packages/api/src/services/users.service.ts
@@ -57,7 +57,9 @@ export class UsersService {
             user.lastName = updateUserRequest.lastName;
         }
         if (updateUserRequest.currentPassword && updateUserRequest.newPassword) {
-            if (!(await this.authService.isUsersPassword(updateUserRequest.currentPassword, updateUserRequest.userId))) {
+            if (
+                !(await this.authService.isUsersPassword(updateUserRequest.currentPassword, updateUserRequest.userId))
+            ) {
                 throw new ForbiddenException(ERROR_CURRENT_PASSWORD_INCORRECT);
             }
             user.password = await this.authService.createNewPassword(updateUserRequest.newPassword);

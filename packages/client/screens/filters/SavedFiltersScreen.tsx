d147ed126181e22758bcc0de2a13f89be09e11d0
Jason Gerard <jasongerard321@gmail.com>
2023-03-08 23:44:08 -0500

    Add slide to delete saved filters

--- packages/client/screens/filters/SavedFiltersScreen.tsx
+++ packages/client/screens/filters/SavedFiltersScreen.tsx
@@ -1,6 +1,6 @@
 import { View } from "../../components/Themed";
-import { FlatList, StyleSheet } from "react-native";
-import { Text, Box, Center, Pressable } from "native-base";
+import { ListRenderItemInfo, StyleSheet } from "react-native";
+import { Text, Box, Center, Pressable, HStack } from "native-base";
 import { HomeStackScreenProps } from "../../types";
 import React from "react";
 import { getFiltersByTransactionStrings } from "../../services/filter_service";
@@ -11,6 +11,7 @@ import { AuthContext } from "../../components/contexts/AuthContext";
 import { FiltersGateway } from "../../gateways/filters_gateway";
 import { Filter } from "@cryptify/common/src/domain/entities/filter";
 import { titleCase } from "@cryptify/common/src/utils/string_utils";
+import { SwipeListView } from "react-native-swipe-list-view";
 
 export default function SavedFiltersScreen({ route, navigation }: HomeStackScreenProps<"SavedFiltersScreen">) {
     const filtersGateway = new FiltersGateway();
@@ -18,6 +19,7 @@ export default function SavedFiltersScreen({ route, navigation }: HomeStackScree
     const isFocused = useIsFocused();
 
     const { token, user } = React.useContext(AuthContext);
+    const [isLoading, setIsLoading] = React.useState(true);
     const [filtersWithHeader, setFiltersWithHeader] = React.useState<FilterWithHeader[]>([]);
 
     React.useEffect(() => {
@@ -51,6 +53,7 @@ export default function SavedFiltersScreen({ route, navigation }: HomeStackScree
                 });
 
                 setFiltersWithHeader(listData);
+                setIsLoading(false);
             }
         })();
     }, [isFocused]);
@@ -76,10 +79,74 @@ export default function SavedFiltersScreen({ route, navigation }: HomeStackScree
         navigation.goBack();
     }
 
-    // TODO use a more efficient way of getting these indicies
-    const stickyHeaderIndices = filtersWithHeader.flatMap((obj) =>
-        obj.header ? [filtersWithHeader.indexOf(obj)] : [],
-    );
+    function renderItem(rowData: ListRenderItemInfo<FilterWithHeader>) {
+        return (
+            <>
+                {rowData.item.header ? (
+                    <Box background={"text.100"}>
+                        <Text
+                            color={"text.500"}
+                            fontWeight={"semibold"}
+                            style={{ paddingHorizontal: 15, paddingVertical: 5 }}
+                        >
+                            {rowData.item.filter.name}
+                        </Text>
+                    </Box>
+                ) : (
+                    <Pressable
+                        onPress={() => onSubmit(rowData.item.filter)}
+                        background={"white"}
+                        _pressed={{ background: "text.200" }}
+                        testID="contactListItem"
+                        height={"46px"}
+                    >
+                        <HStack height={"100%"}>
+                            <Text style={{ paddingHorizontal: 15, paddingVertical: 10 }}>
+                                {rowData.item.filter.name}
+                            </Text>
+                        </HStack>
+                    </Pressable>
+                )}
+            </>
+        );
+    }
+
+    function renderHiddenItem(rowData: ListRenderItemInfo<FilterWithHeader>) {
+        return (
+            !rowData.item.header && (
+                <Pressable
+                    onPress={async () => {
+                        try {
+                            await filtersGateway.deleteFilter(
+                                {
+                                    id: user.id,
+                                    name: rowData.item.filter.name,
+                                    currencyType: route.params.currencyType,
+                                },
+                                token,
+                            );
+
+                            const arr = [...filtersWithHeader];
+                            const index = filtersWithHeader.findIndex(
+                                (item) => item.filter.name === rowData.item.filter.name,
+                            );
+                            arr.splice(index, 1);
+                            setFiltersWithHeader(arr);
+                        } catch (e) {}
+                    }}
+                    style={[styles.deleteButton]}
+                    backgroundColor={"red.500"}
+                    height={"46px"}
+                >
+                    <Text color={"white"}>Delete</Text>
+                </Pressable>
+            )
+        );
+    }
+
+    if (isLoading) {
+        return;
+    }
 
     return (
         <View style={styles.view}>
@@ -92,34 +159,14 @@ export default function SavedFiltersScreen({ route, navigation }: HomeStackScree
                     </Text>
                 </Center>
             ) : (
-                <FlatList
+                <SwipeListView
                     data={filtersWithHeader}
-                    renderItem={({ item }) => (
-                        <>
-                            {item.header ? (
-                                <Box background={"text.100"}>
-                                    <Text
-                                        color={"text.500"}
-                                        fontWeight={"semibold"}
-                                        style={{ paddingHorizontal: 15, paddingVertical: 5 }}
-                                    >
-                                        {item.filter.name}
-                                    </Text>
-                                </Box>
-                            ) : (
-                                <Pressable
-                                    onPress={() => onSubmit(item.filter)}
-                                    _pressed={{ background: "text.200" }}
-                                    testID="contactListItem"
-                                >
-                                    <Text style={{ paddingHorizontal: 15, paddingVertical: 10 }}>
-                                        {item.filter.name}
-                                    </Text>
-                                </Pressable>
-                            )}
-                        </>
-                    )}
-                    stickyHeaderIndices={stickyHeaderIndices}
+                    renderItem={renderItem}
+                    renderHiddenItem={renderHiddenItem}
+                    rightOpenValue={-74}
+                    disableRightSwipe={true}
+                    previewRowKey={"0"}
+                    friction={100}
                 />
             )}
         </View>
@@ -142,5 +189,12 @@ const styles = StyleSheet.create({
         textAlign: "center",
         maxWidth: 265,
         marginTop: 15,
+    },
+    deleteButton: {
+        alignItems: "center",
+        justifyContent: "center",
+        position: "absolute",
+        right: 0,
+        width: 74,
     },
 });

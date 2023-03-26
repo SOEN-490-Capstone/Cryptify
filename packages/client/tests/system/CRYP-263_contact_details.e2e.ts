11f00b5ee8d2689e89305e9f2039adbe7237dc79
Jason Gerard <jasongerard321@gmail.com>
2023-03-26 13:57:16 -0400

    Sys test 263

---
+++ packages/client/tests/system/CRYP-263_contact_details.e2e.ts
@@ -0,0 +1,30 @@
+import { openAppForDebugBuild } from "./utils/open_app_for_debug_build";
+import { by, device, expect, element } from "detox";
+import { pause } from "./utils/test_utils";
+
+describe("CRYP-263 Contacts details", () => {
+    beforeEach(async () => {
+        await device.launchApp({
+            newInstance: true,
+        });
+        await openAppForDebugBuild();
+    });
+
+    it("Should be able to view the details of a users contacts", async () => {
+        // Sign in
+        await element(by.id("signInButton")).tap();
+        await expect(element(by.text("Welcome back"))).toBeVisible();
+        await element(by.id("email")).typeText("john@example.com");
+        await element(by.id("password")).typeText("Test123!");
+        await element(by.text("Sign in")).tap();
+
+        // Nav to contacts screen
+        await element(by.id("settingsTab")).tap();
+        await element(by.id("contactsButton")).tap();
+        await element(by.id("contactListItem")).atIndex(0).tap();
+
+        await expect(element(by.text("Jason"))).toBeVisible();
+        
+        await pause();
+    });
+});

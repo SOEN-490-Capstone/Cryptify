c47cd353868b1cd4ea867ff658386eea89230e0e
Jason Gerard <jasongerard321@gmail.com>
2023-03-26 14:08:08 -0400

    Add sys test for user account 202 203 204

---
+++ packages/client/tests/system/CRYP-204_delete_account.e2e.ts
@@ -0,0 +1,30 @@
+import { openAppForDebugBuild } from "./utils/open_app_for_debug_build";
+import { by, device, expect, element } from "detox";
+import { pause } from "./utils/test_utils";
+
+describe("CRYP-204 Delete user account", () => {
+    beforeEach(async () => {
+        await device.launchApp({
+            newInstance: true,
+        });
+        await openAppForDebugBuild();
+    });
+
+    it("Should be able to delete a user account", async () => {
+        // Sign in
+        await element(by.id("signInButton")).tap();
+        await expect(element(by.text("Welcome back"))).toBeVisible();
+        await element(by.id("email")).typeText("john@example.com");
+        await element(by.id("password")).typeText("Test123!");
+        await element(by.text("Sign in")).tap();
+
+        // Nav to contacts screen
+        await element(by.id("settingsTab")).tap();
+        await element(by.id("accountButton")).tap();
+        
+        await element(by.id("deleteAccountButton")).tap();
+        await element(by.text("DELETE")).tap();
+
+        await pause();
+    });
+});

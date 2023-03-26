d335cb16de4a8e7fda3165d70a881fc1fbf45b2f
Jason Gerard <jasongerard321@gmail.com>
2023-03-26 14:52:52 -0400

    Contact system test 192 191

---
+++ packages/client/tests/system/CRYP-191_edit_contact.e2e.ts
@@ -0,0 +1,31 @@
+import { openAppForDebugBuild } from "./utils/open_app_for_debug_build";
+import { by, device, expect, element } from "detox";
+import { pause } from "./utils/test_utils";
+
+describe("CRYP-191 Edit contact", () => {
+    beforeEach(async () => {
+        await device.launchApp({
+            newInstance: true,
+        });
+        await openAppForDebugBuild();
+    });
+
+    it("Should be able to edit a contact", async () => {
+        // Sign in
+        await element(by.id("signInButton")).tap();
+        await expect(element(by.text("Welcome back"))).toBeVisible();
+        await element(by.id("email")).typeText("john@example.com");
+        await element(by.id("password")).typeText("Test123!");
+        await element(by.text("Sign in")).tap();
+
+        // Nav to contacts screen
+        await element(by.id("settingsTab")).tap();
+        await element(by.id("contactsButton")).tap();
+        await element(by.id("contactListItem")).atIndex(0).tap();
+        await element(by.text("Edit")).tap();
+
+        await expect(element(by.text("Edit a Contact"))).toBeVisible();
+        
+        await pause();
+    });
+});
---
+++ packages/client/tests/system/CRYP-192_delete_contact.e2e.ts
@@ -0,0 +1,32 @@
+import { openAppForDebugBuild } from "./utils/open_app_for_debug_build";
+import { by, device, expect, element } from "detox";
+import { pause } from "./utils/test_utils";
+
+describe("CRYP-192 Delete contact", () => {
+    beforeEach(async () => {
+        await device.launchApp({
+            newInstance: true,
+        });
+        await openAppForDebugBuild();
+    });
+
+    it("Should be able to delete a contact", async () => {
+        // Sign in
+        await element(by.id("signInButton")).tap();
+        await expect(element(by.text("Welcome back"))).toBeVisible();
+        await element(by.id("email")).typeText("john@example.com");
+        await element(by.id("password")).typeText("Test123!");
+        await element(by.text("Sign in")).tap();
+
+        // Nav to contacts screen
+        await element(by.id("settingsTab")).tap();
+        await element(by.id("contactsButton")).tap();
+        await element(by.id("contactListItem")).atIndex(0).tap();
+        await element(by.text("Edit")).tap();
+        
+        await element(by.text("Delete contact")).tap();
+        await element(by.text("DELETE")).tap();
+        
+        await pause();
+    });
+});

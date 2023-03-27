4f28b16a06ac844652aed26f8b738ac00df96810
Michael Warner <michael.gregory.warner@gmail.com>
2023-03-27 10:23:06 -0400

    pr comments

--- packages/client/screens/account/AccountScreen.tsx
+++ packages/client/screens/account/AccountScreen.tsx
@@ -55,7 +55,7 @@ export default function AccountScreen({ navigation }: SettingsStackScreenProps<"
                     inlineLink={() => navigation.navigate("AccountTypeScreen")}
                 />
                 <Pressable onPress={() => navigation.navigate("AccountPasswordScreen")}>
-                    <HStack alignItems="center">
+                    <HStack height={"44px"} alignItems="center">
                         <Text>Change Password</Text>
                         <FontAwesomeIcon icon={farChevronRight} style={styles.chevronRightIcon} size={16} />
                     </HStack>

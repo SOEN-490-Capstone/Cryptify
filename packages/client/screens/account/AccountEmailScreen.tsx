2902afcc1d23c847122b199b6f095ffe089d274c
Michael Warner <michael.gregory.warner@gmail.com>
2023-03-28 20:32:17 -0400

    pr comments

--- packages/client/screens/account/AccountEmailScreen.tsx
+++ packages/client/screens/account/AccountEmailScreen.tsx
@@ -64,41 +64,47 @@ export default function AccountNameScreen() {
         <View style={styles.view}>
             <Formik initialValues={intitialValues} validationSchema={updateUserSchema} onSubmit={handleUpdate}>
                 {({ values, errors, touched, handleChange, submitForm }) => (
-                    <VStack space={4}>
-                        <Text size={"title2"} fontWeight={"semibold"}>
-                            Current
-                        </Text>
-                        <Text>{user.email}</Text>
-                        <Text size={"title2"} marginTop={5} fontWeight={"semibold"}>
-                            New
-                        </Text>
-                        <FormControl isInvalid={!!(errors.email && touched.email)}>
-                            <Input
-                                value={values.email}
-                                onChangeText={handleChange("email")}
-                                placeholder="Email"
-                                testID="email"
-                            />
-                            <FormControl.ErrorMessage>{touched.email && errors.email}</FormControl.ErrorMessage>
-                        </FormControl>
-                        <FormControl isInvalid={!!(errors.confirmEmail && touched.confirmEmail)}>
-                            <Input
-                                value={values.confirmEmail}
-                                onChangeText={handleChange("confirmEmail")}
-                                placeholder="Confirm email"
-                                testID="confirmEmail"
-                            />
-                            <FormControl.ErrorMessage>
-                                {touched.confirmEmail && errors.confirmEmail}
-                            </FormControl.ErrorMessage>
-                            <Button
-                                isDisabled={!(values.confirmEmail && values.email)}
-                                style={styles.Button}
-                                onPress={submitForm}
-                            >
-                                Save changes
-                            </Button>
-                        </FormControl>
+                    <VStack space={"35px"}>
+                        <VStack space={"20px"}>
+                            <Text size={"title3"} fontWeight={"semibold"}>
+                                Current
+                            </Text>
+                            <Text>{user.email}</Text>
+                        </VStack>
+                        <VStack>
+                            <Text size={"title3"} fontWeight={"semibold"}>
+                                New
+                            </Text>
+                            <VStack space={"13px"} marginTop={"15px"}>
+                                <FormControl isInvalid={!!(errors.email && touched.email)}>
+                                    <Input
+                                        value={values.email}
+                                        onChangeText={handleChange("email")}
+                                        placeholder="Email"
+                                        testID="email"
+                                    />
+                                    <FormControl.ErrorMessage>{touched.email && errors.email}</FormControl.ErrorMessage>
+                                </FormControl>
+                                <FormControl isInvalid={!!(errors.confirmEmail && touched.confirmEmail)}>
+                                    <Input
+                                        value={values.confirmEmail}
+                                        onChangeText={handleChange("confirmEmail")}
+                                        placeholder="Confirm email"
+                                        testID="confirmEmail"
+                                    />
+                                    <FormControl.ErrorMessage>
+                                        {touched.confirmEmail && errors.confirmEmail}
+                                    </FormControl.ErrorMessage>
+                                </FormControl>
+                                <Button
+                                    isDisabled={!(values.confirmEmail && values.email)}
+                                    onPress={submitForm}
+                                    marginTop={"7px"}
+                                >
+                                    Save changes
+                                </Button>
+                            </VStack>
+                        </VStack>
                     </VStack>
                 )}
             </Formik>
@@ -110,14 +116,7 @@ const styles = StyleSheet.create({
     view: {
         flex: 1,
         paddingHorizontal: 15,
-        paddingTop: 10,
-    },
-    Button: {
-        marginTop: 20,
-    },
-    ButtonDisabled: {
-        opacity: 0.6,
-        marginTop: 20,
+        paddingTop: 20,
     },
     toastBox: {
         backgroundColor: "#404040",

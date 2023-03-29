b53f90f1b2171730bea817dac52ed77c2a7ea86b
Domenic Seccareccia <dom_seccareccia@hotmail.com>
2023-03-29 11:34:23 -0400

    Add another Bitcoin/Ethereum wallet button displayed according to specs
    - Add another Bitcoin/Ethereum wallet button will now only display if at least 1 form field is present for their respective sections
    - Fixed Add Contact UI to match UI mockups
    - Added missing error messages for the contact name form field

--- packages/common/src/errors/error_messages.ts
+++ packages/common/src/errors/error_messages.ts
@@ -15,6 +15,9 @@ export const ERROR_TAG_NAME_ALREADY_EXIST = "This tag already exists.";
 export const ERROR_TAG_NOT_FOUND = "Transaction tag not found.";
 export const ERROR_TRANSACTIONS_NOT_FOUND = "One or more transactions not found.";
 
+export const ERROR_CONTACT_NAME_EMPTY = "Enter a name.";
+export const ERROR_CONTACT_NAME_TOO_LONG = "Name must be 65 characters or less.";
+export const ERROR_CONTACT_NAME_INVALID_CHARACTERS = "Name must only contain alphabetic characters and spaces.";
 export const ERROR_CONTACT_NAME_ALREADY_ADDED_TO_ACCOUNT = "This name is already associated with another contact.";
 export const ERROR_ADDRESS_ALREADY_ADDED_TO_CONTACT =
     "This Bitcoin wallet address is already associated with another contact.";
